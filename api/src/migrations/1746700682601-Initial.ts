ad8eca058e8ed0456755bd63a2a966c5fab2029f
artemkolodko <temakolodko@gmail.com>
2025-05-08 13:05:07 +0200

    update migrations

--- api/src/migrations/1746700682601-Initial.ts
+++ api/src/migrations/1746700682601-Initial.ts
@@ -4,9 +4,9 @@ export class Initial1746700682601 implements MigrationInterface {
     name = 'Initial1746700682601'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "portfolio_snapshots" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "version" character varying NOT NULL, "walletAddress" character varying NOT NULL, "data" json NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_46c13ef40300b3a6d379488f53a" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE IF NOT EXISTS "portfolio_snapshots" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "version" character varying NOT NULL, "walletAddress" character varying NOT NULL, "data" json NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_46c13ef40300b3a6d379488f53a" PRIMARY KEY ("id"))`);
         await queryRunner.query(`
-            CREATE INDEX "IDX_walletAddress" ON "portfolio_snapshots" ("walletAddress")
+            CREATE INDEX IF NOT EXISTS "IDX_walletAddress" ON "portfolio_snapshots" ("walletAddress")
         `);
     }
 
